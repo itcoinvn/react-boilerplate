b03b20ee25106b1eb6d4cd41472f739ce7f4b813
Duc Pham <kaivnit@gmail.com>
2022-04-13 18:47:04 +0700

    chore: update root element define

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root') as HTMLElement;
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = createRoot(rootElement);
